6ec3f482e3c12d8d6aaaad6b8fb1a9a4e9d541ad
Brandon McLean <brandonmclean@live.com>
2020-05-05 11:38:32 -0400

    Updated changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,50 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react',
+  'faker',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk',
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
 };
